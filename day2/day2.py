5df4d4ccd9430f4a7b67f8e63f975d71d567655d
vintagefuture <pinkpallyn@gmail.com>
2024-12-02 23:15:38 +0000

    completed day2 part 2
    Change-Id: Idd7cdbdd4ef5bb101588e81561316ad1d3b37d36

--- day2/day2.py
+++ day2/day2.py
@@ -2,30 +2,56 @@ reports = []
 safe_reports = []
 
 def is_safe(report):
-    max_iterations = len(report) - 1
+    max_iterations = len(report)-1
     increasing = report[1] > report[0]
-    strikes = 0
-    print(f'This is report {report}')
+
+    strike = False
+    bad_index = 0
+
     for i in range(max_iterations):
-        too_much_interval = False
-        not_ordered = False
         if not abs(report[i] - report[i+1]) in [1, 2, 3]:
-            too_much_interval = True
+            bad_index = i
+            strike = True
+            break
 
         if (report[i + 1] > report[i]) != increasing:
-            not_ordered = True
+            bad_index = i
+            strike = True
+            break
 
-        non_compliant = too_much_interval or not_ordered
-        print(f'{report[i]} and {report[i+1]}: {non_compliant}')
-        if non_compliant:
-            strikes += 1
-    print(strikes)
-    if strikes < 2:
-        print(f'This report is safe')
-        return True
-    else:
-        print(f'This report is not safe')
-        return False
+    if strike:
+        print(f'Initial report was {report}')
+        fixed_report1 = [i for i in report]
+        fixed_report1.pop(bad_index)
+        print(f'Report is now {fixed_report1}')
+
+        first_option_failed = False
+
+        max_iterations = len(fixed_report1) - 1
+        increasing = fixed_report1[1] > fixed_report1[0]
+        for i in range(max_iterations):
+            if not abs(fixed_report1[i] - fixed_report1[i+1]) in [1, 2, 3]:
+                first_option_failed = True
+
+            if (report[i + 1] > report[i]) != increasing:
+                first_option_failed = True
+
+        if first_option_failed:
+            print(f'The first option failed, try second')
+            fixed_report2 = [i for i in report]
+            fixed_report2.pop(bad_index+1)
+            print(f'New report is {fixed_report2}')
+
+            increasing = fixed_report2[1] > fixed_report2[0]
+            for i in range(max_iterations):
+                if not abs(fixed_report2[i] - fixed_report2[i + 1]) in [1, 2, 3]:
+                    return False
+
+                if (report[i + 1] > report[i]) != increasing:
+                    return False
+
+    return True
+
 
 def main():
     with open("input.txt") as f:
