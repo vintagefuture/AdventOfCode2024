a9c629e428364c4060d630ee438546d2c58a453b
vintagefuture <pinkpallyn@gmail.com>
2024-12-08 11:26:24 +0000

    working on day 6
    Change-Id: I4c4bdef446e8ec0b0261e30145157f70a8b8f682

---
+++ day6/day6_part1.py
@@ -0,0 +1,44 @@
+def get_obstacles(file_path):
+    obstacles_coordinates = []
+    with open(file_path, 'r') as file:
+        for line_y, line in (enumerate(file)):
+            for i, j in enumerate(line):
+                if j == '#':
+                    obstacles_coordinates.append([i, line_y])
+                elif j == '^':
+                    guard_initial_position = [i, line_y]
+
+    return obstacles_coordinates, guard_initial_position
+
+
+def check_obstacle(obstacles_coordinates, initial_position):
+    direction = 'up'
+    next_coordinates = go_forward(initial_position, direction)
+    if next_coordinates in obstacles_coordinates:
+        return True
+
+    return False
+
+def go_forward(initial_position, direction):
+    next_position = []
+    up_position = [initial_position[0], initial_position[1] - 1]
+    down_position = [initial_position[0], initial_position[1] + 1]
+    right_position = [initial_position[0] + 1, initial_position[1]]
+    left_position = [initial_position[0] - 1, initial_position[1]]
+    if direction == 'up':
+        next_position = up_position
+    elif direction == 'down':
+        next_position = down_position
+    elif direction == 'left':
+        next_position = left_position
+    elif direction == 'right':
+        next_position = right_position
+
+    return next_position
+
+def main():
+    obstacles_coordinates, initial_position = get_obstacles("test_input.txt")
+
+
+if __name__ == '__main__':
+    main()
