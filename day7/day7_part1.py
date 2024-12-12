6140b38edac7b0daeb24a18c5dfe95deafe61623
vintagefuture <pinkpallyn@gmail.com>
2024-12-12 19:00:52 +0000

    day 7 part1 complete and polished
    Change-Id: Icc356c98c05814bd2041cfb1465c99fae5e09545

--- day7/day7_part1.py
+++ day7/day7_part1.py
@@ -1,4 +1,3 @@
-import operator
 import itertools
 from itertools import chain, zip_longest
 
@@ -10,23 +9,13 @@ def parse_file(file_path):
             rows.append(row)
     return rows
 
-ops = {
-    '+' : operator.add,
-    '*' : operator.mul,
-}
-
 def all_combinations(length):
     return [p for p in itertools.product(['+', '*'], repeat=length)]
-
-def eval_binary_expr(op1, oper, op2):
-    op1, op2 = int(op1), int(op2)
-    return ops[oper](op1, op2)
 
 def intertwine(l1, l2):
     return [x for x in chain.from_iterable(zip_longest(l1, l2)) if x is not None]
 
-
-if __name__ == '__main__':
+def main():
     equations = parse_file('input.txt')
     total = []
     for equation in equations:
@@ -39,32 +28,14 @@ if __name__ == '__main__':
         for result in results:
             while len(result) > 1:
                 temp_sum = 0
-                print(f'Expression to evaluate is {result}')
-                print("Consider the first operation")
                 first_slice = result[:3]
-                print(f'First slice {first_slice}')
-                print(f"I'm going to evaluate {' '.join(first_slice)} which returns {eval(' '.join(first_slice))}")
                 temp_sum += eval(' '.join(first_slice))
                 del result[0:3]
-                print(f"I removed the slice so that the resulting list is {result}")
                 result.insert(0, str(temp_sum))
-                print(f"And now I re-added the result of the first slice which is {str(temp_sum)} so that the resulting list is now {result}")
             if temp_sum == target:
                 total.append(temp_sum)
-                print(f'and we have a match with {temp_sum}')
                 break
-            print(f"I stopped since I reached the end! The final temp_sum is {temp_sum}")
+    print(f"The total is {sum(total)}")
 
-        print(f"The total is {sum(total)}")
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
