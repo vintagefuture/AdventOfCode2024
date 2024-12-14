51a9be3c3760cf2336933dd6c6882b2cae0bbd74
vintagefuture <pinkpallyn@gmail.com>
2024-12-14 23:10:41 +0100

    misread the assignment for part2, do over ... :-(
    Change-Id: Ie14910e96dfc62475df81bb6a4b326d7291c898c

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -1,19 +1,3 @@
-# --- Part Two ---
-#
-# The engineers seem concerned; the total calibration result you gave them is nowhere close to being within safety tolerances. Just then, you spot your mistake: some well-hidden elephants are holding a third type of operator.
-#
-# The concatenation operator (||) combines the digits from its left and right inputs into a single number. For example, 12 || 345 would become 12345. All operators are still evaluated left-to-right.
-#
-# Now, apart from the three equations that could be made true using only addition and multiplication, the above example has three more equations that can be made true by inserting operators:
-#
-#     156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
-#     7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
-#     192: 17 8 14 can be made true using 17 || 8 + 14.
-#
-# Adding up all six test values (the three that could be made before using only + and * plus the new three that can now be made by also using ||) produces the new total calibration result of 11387.
-#
-# Using your new knowledge of elephant hiding spots, determine which equations could possibly be true. What is their total calibration result?
-
 import itertools
 from itertools import chain, zip_longest
 
@@ -43,29 +27,30 @@ def find_additional_equations(equations):
             additional_equations.append(local_equation)
     return additional_equations
 
-def check_equations(equations):
+def check_equations (equations: list[list[str]]) -> [int, list[list], list[list]]:
     total = []
     equations_left_to_try = equations.copy()
+    sorted = []
     for equation in equations:
-        if len(equation) > 2:
-            possible_operators = all_combinations(len(equation) - 2)
-            target = int(equation[0])
-            numbers = equation[1:]
-            results = []
-            for option in possible_operators:
-                results.append(intertwine(numbers, option))
-            for result in results:
-                while len(result) > 1:
-                    temp_sum = 0
-                    first_slice = result[:3]
-                    temp_sum += eval(' '.join(first_slice))
-                    del result[0:3]
-                    result.insert(0, str(temp_sum))
-                if temp_sum == target:
-                    equations_left_to_try.remove(equation)
-                    total.append(temp_sum)
-                    break
-    return {"total_sum": sum(total), "still_to_try": equations_left_to_try}
+        possible_operators = all_combinations(len(equation) - 2)
+        target = int(equation[0])
+        numbers = equation[1:]
+        results = []
+        for option in possible_operators:
+            results.append(intertwine(numbers, option))
+        for result in results:
+            while len(result) > 1:
+                temp_sum = 0
+                first_slice = result[:3]
+                temp_sum += eval(' '.join(first_slice))
+                del result[0:3]
+                result.insert(0, str(temp_sum))
+            if temp_sum == target:
+                equations_left_to_try.remove(equation)
+                sorted.append(equation)
+                total.append(temp_sum)
+                break
+    return [sum(total), sorted, equations_left_to_try]
 
 def count_single_values(equations):
     total = 0
@@ -76,13 +61,27 @@ def count_single_values(equations):
 
 
 if __name__ == '__main__':
+    # parse input file
     equations = parse_file('test_input.txt')
-    print(f"First round with {equations}")
-    result = check_equations(equations)["total_sum"]
-    print(f"The result is {result}")
-    additional_equations = find_additional_equations((check_equations(equations)["still_to_try"]))
-    print(f"The remaining equations are {additional_equations}")
-    result += count_single_values(additional_equations)
-    print(result)
-    result += check_equations(additional_equations)["total_sum"]
-    print(result)
+    print(equations)
+
+    # first round
+    result = check_equations(equations)
+    print(f"The result is {result[0]}")
+    print(f"Sorted equations: {result[1]}")
+    print(f"Left to try: {result[2]}")
+
+    # second round
+    more_stuff = find_additional_equations(result[2])
+    print(more_stuff)
+    singles = count_single_values(more_stuff)
+    print(singles)
+    multiples = (list(filter(lambda x: len(x) >2, more_stuff)))
+    print(f"Checking on multiples {multiples}")
+    additional_equations = check_equations(multiples)
+    print(additional_equations[0])
+    # print(f"I've found an additional {additional_equations[0]}")
+    # result += count_single_values(additional_equations)
+    # print(result)
+    # result += check_equations(additional_equations)["total_sum"]
+    # print(result)
